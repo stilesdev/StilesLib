c47ad5f6e37d2de3550ff13b65fa4a59c62aa9b6
mstiles92 <mstiles92@gmail.com>
2014-04-03 01:48:25 -0400

    Update JavaDoc comments for CalendarUtils.

--- src/main/java/com/mstiles92/plugins/commonutils/calendar/CalendarUtils.java
+++ src/main/java/com/mstiles92/plugins/commonutils/calendar/CalendarUtils.java
@@ -1,3 +1,21 @@
+/**
+ * This document is a part of the source code and related artifacts for CommonUtils, an open source library that
+ * provides a set of commonly-used functions for Bukkit plugins.
+ *
+ * http://github.com/mstiles92/CommonUtils
+ *
+ * Copyright (c) 2014 Matthew Stiles (mstiles92)
+ *
+ * Licensed under the Common Development and Distribution License Version 1.0
+ * You may not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the CDDL-1.0 License at http://opensource.org/licenses/CDDL-1.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the license.
+ */
+
 package com.mstiles92.plugins.commonutils.calendar;
 
 import java.util.Calendar;
@@ -66,6 +84,13 @@ public class CalendarUtils {
         return calendar;
     }
 
+    /**
+     * Build up a String representing the difference in time between two Calendar objects.
+     *
+     * @param first the first Calendar object
+     * @param second the second Calendar object
+     * @return the time difference between the two Calendar objects
+     */
     public static String buildTimeDifference(Calendar first, Calendar second) {
         if (first.equals(second)) {
             return "now";
@@ -87,6 +112,14 @@ public class CalendarUtils {
         return s.toString().trim();
     }
 
+    /**
+     * Get the difference in time between two calendars for an individual time unit.
+     *
+     * @param constant the constant representing the time unit to compare
+     * @param first the first Calendar object
+     * @param second the second Calendar object
+     * @return the number of the specified time units between the first and second Calendar objects
+     */
     private static int getDifference(int constant, Calendar first, Calendar second) {
         int difference = 0;
         Calendar temp = new GregorianCalendar();
